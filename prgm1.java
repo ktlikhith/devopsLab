400040c941786a5acc652a69b5cfbc8c48f62d09
K T Likhith <ktlikhith.naik22@gmail.com>
2022-10-04 14:47:54 +0530

    added

--- prgm1.java
+++ prgm1.java
@@ -1,6 +1,7 @@
 class HelloWorld{
 public static void main(String[],args){
 	int a=10;
+	int b=100;
 System.out.println("Hello World");
 System.out.println("aaaa");
 System.out.println("From mashood");
